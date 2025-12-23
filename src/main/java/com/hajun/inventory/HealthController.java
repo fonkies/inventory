afa8b66360bebeaa0ea897ccf41a1026f4df1663
HAJUN <fonkies@naver.com>
2025-12-23 16:54:48 +0900

    chore: simplify heath endpoint

--- src/main/java/com/hajun/inventory/HealthController.java
+++ src/main/java/com/hajun/inventory/HealthController.java
@@ -1,27 +1,15 @@
 package com.hajun.inventory;
 
-import com.hajun.inventory.global.exception.ConflictException;
-import com.hajun.inventory.common.exception.NotFoundException;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HealthController {
-
     @GetMapping("/health")
     public String health() {
         return "ok";
     }
-    @GetMapping("/boom/notfound")
-    public String nf() {
-        throw new NotFoundException("NOT_FOUND_TEST", "없음 테스트");
-
-    }
-
-    @GetMapping("/boom/conflict")
-    public String cf() {
-        throw new ConflictException("CONFLICT_TEST", "중복 테스트");
-
-    }
-
 }
+
+
+
